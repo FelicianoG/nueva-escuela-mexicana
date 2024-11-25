893781a8649d8de9aca1e0841141f58795028bf3
FelicianoG <felicianogc@gmail.com>
2024-11-24 22:38:37 -0700

    Fixed issues with typescript types and warning cleanup

--- src/pages/home/components/Conferencista.tsx
+++ src/pages/home/components/Conferencista.tsx
@@ -9,22 +9,90 @@ type Props = {
   imageSize?: string;
 };
 
+interface ConferencistaStylesProps {
+  position: number[],
+  imagen: string,
+  imageSize: string | undefined
+}
+
 export default function Conferencista({ titulo, imagen, nombre, cargo, position = [0, 0], imageSize }: Props) {
-  const ConferencistaStyles = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: start;
-    align-items: center;
-    width: 100%;
-    margin: 30px;
-    @media (max-width: 800px) {
-      padding-bottom: 30px;
-      border-bottom: solid white 2px;
-      margin: 0;
-      &:last-child {
-        border-bottom: none;
-      }
+
+  return (
+    <ConferencistaStyles position={position} imagen={imagen} imageSize={imageSize} id="conferencista-styles">
+      <h1>{titulo}</h1>
+      <div className="imagen"></div>
+      {/* <img src={imagen} alt="conferencia" /> */}
+      <h2>{nombre}</h2>
+      <h3>{cargo}</h3>
+    </ConferencistaStyles>
+  );
+}
+
+const ConferencistaStyles = styled.div<ConferencistaStylesProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  width: 100%;
+  margin: 30px;
+
+  @media (max-width: 800px) {
+    padding-bottom: 30px;
+    border-bottom: solid white 2px;
+    margin: 0;
+    &:last-child {
+      border-bottom: none;
     }
+  }
+
+  h1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    line-height: 20px;
+    max-width: 20em;
+    margin-bottom: 10px;
+    font-family: "Lulo";
+    color: white;
+    font-size: medium;
+    min-height: 80px;
+  }
+
+  h2 {
+    text-align: center;
+    line-height: 20px;
+    max-width: 20em;
+    margin-bottom: 10px;
+    font-family: "Lulo-Bold";
+    font-size: medium;
+    color: white;
+  }
+
+  h3 {
+    text-align: center;
+    line-height: 20px;
+    max-width: 20em;
+    margin-bottom: 10px;
+    font-family: "Lulo";
+    font-size: small;
+    color: white;
+  }
+
+  .imagen {
+    width: 200px;
+    height: 200px;
+    margin: 20px;
+    background-position: 0px 0px;
+    border-radius: 100%;
+    max-width: 200px;
+    background-size: cover;
+    background-position: ${(props) => `${props.position[0]}px ${props.position[1]}px`};
+    background-image: ${(props) => `url(${props.imagen})`};
+    background-size: ${(props) => props.imageSize ? props.imageSize : "cover"};
+  }
+  
+  @media(max-width: 800px) {
     h1 {
       display: flex;
       justify-content: center;
@@ -35,7 +103,8 @@ export default function Conferencista({ titulo, imagen, nombre, cargo, position 
       margin-bottom: 10px;
       font-family: "Lulo";
       color: white;
-      font-size: medium;
+      margin-top: 1rem;
+      font-size: small;
       min-height: 80px;
     }
     h2 {
@@ -46,6 +115,7 @@ export default function Conferencista({ titulo, imagen, nombre, cargo, position 
       font-family: "Lulo-Bold";
       font-size: medium;
       color: white;
+      max-width: 16rem;
     }
     h3 {
       text-align: center;
@@ -57,70 +127,14 @@ export default function Conferencista({ titulo, imagen, nombre, cargo, position 
       color: white;
     }
     .imagen {
-      width: 200px;
-      height: 200px;
+      width: 10rem;
+      height: 10rem;
       margin: 20px;
-      background-position: 0px 0px;
       border-radius: 100%;
       max-width: 200px;
-      background-size: cover;
-      background-position: ${`${position[0]}px`} ${`${position[1]}px`};
-      background-image: ${`url(${imagen})`};
-      background-size: ${imageSize ? imageSize : "cover"};
+      background-position: ${(props) => `${props.position[0] - 15}px ${props.position[1]}px`};
+      background-image: ${(props) => `url(${props.imagen})`};
+      background-size: ${(props) => props.imageSize ? props.imageSize : "cover"};
     }
-    @media (max-width: 800px) {
-      h1 {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-        line-height: 20px;
-        max-width: 20em;
-        margin-bottom: 10px;
-        font-family: "Lulo";
-        color: white;
-        margin-top: 1rem;
-        font-size: small;
-        min-height: 80px;
-      }
-      h2 {
-        text-align: center;
-        line-height: 20px;
-        max-width: 20em;
-        margin-bottom: 10px;
-        font-family: "Lulo-Bold";
-        font-size: medium;
-        color: white;
-        max-width: 16rem;
-      }
-      h3 {
-        text-align: center;
-        line-height: 20px;
-        max-width: 20em;
-        margin-bottom: 10px;
-        font-family: "Lulo";
-        font-size: small;
-        color: white;
-      }
-      .imagen {
-        width: 10rem;
-        height: 10rem;
-        margin: 20px;
-        border-radius: 100%;
-        max-width: 200px;
-        background-position: ${`${position[0] - 15}px`} ${`${position[1]}px`};
-        background-image: ${`url(${imagen})`};
-        background-size: ${imageSize ? imageSize : "cover"};
-      }
-    }
-  `;
-  return (
-    <ConferencistaStyles id="conferencista-styles">
-      <h1>{titulo}</h1>
-      <div className="imagen"></div>
-      {/* <img src={imagen} alt="conferencia" /> */}
-      <h2>{nombre}</h2>
-      <h3>{cargo}</h3>
-    </ConferencistaStyles>
-  );
-}
+  }
+`;
--- src/pages/home/components/Violentometro.tsx
+++ src/pages/home/components/Violentometro.tsx
@@ -1,12 +1,166 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+
+const ThermometerApp = () => {
+    const config = {
+        minTemp: 0,
+        maxTemp: 50,
+    };
+
+    const topicList = [
+        ['trata', 'Trata virtual de personas'],
+        ['difusion', 'Difusión de contenido íntimo sin consentimiento'],
+        ['sextorsion', 'Sextorsión'],
+        ['difamacion', 'Difamación virtual'],
+        ['suplantacion', 'Suplantación virtual'],
+        ['hostigamiento', 'Hostigamiento virtual'],
+        ['acecho', 'Acecho virtual'],
+        ['insultos', 'Insultos electrónicos'],
+    ]
+
+    const termoDescriptions = {
+        trata: {
+            title: "Trata virtual de personas",
+            description:
+                "Conlleva la intermediación de las tecnologías para el ejercicio de poder sobre una víctima con el objeto de cometer abuso o explotación sexual de su imagen y/o de su cuerpo contra su voluntad. Uso de juegos en línea para atraer víctimas. Enganche y manipulación emocional. Seleccionan a víctimas más vulnerables: mujeres, niños, niñas."
+        },
+        difusion: {
+            title: "Difusión de contenido íntimo sin consentimiento",
+            description:
+                "Consiste en la difusión en línea no consensuada de imágenes íntimas obtenidas con o sin el consentimiento de la persona, con el propósito de avergonzar, estigmatizar o perjudicar a la víctima. Existencia de material audiovisual (real o editado) de carácter íntimo y/o sexual. Creación, almacenamiento, manipulación, producción, difusión, publicación, distribución, facilitación, cesión o entrega a terceros de este material."
+        },
+        sextorsion: {
+            title: "Sextorsión",
+            description:
+                "Implica el envío o publicación de comunicaciones o contenidos digitales que le anticipan a una persona la intención de cometer en su contra un daño físico o violencia sexual, o en contra de sus familiares, amistades o bienes. Ataques sexuales organizados o planificados mediante el uso de tecnologías, redes o sitios de citas. Presión sobre una persona para forzarla a actuar de cierto modo, obtener dinero, y/o amenazas de difundir información privada para chantaje sexual."
+        },
+        difamacion: {
+            title: "Difamación virtual",
+            description:
+                "Implica la creación, manipulación y publicación de información personal falsa, manipulada o fuera de contexto con la intención de descalificar o dañar la reputación de una persona. Creación de perfiles falsos en redes sociales o sitios en línea para afectar la reputación de la víctima. Publicación de publicidad falsa en sitios de citas o pornográficos. Uso y manipulación no consentida de información personal (fotos y videos)."
+        },
+        suplantacion: {
+            title: "Suplantación virtual",
+            description:
+                "Consiste en la utilización de la imagen, información o datos de una persona o la creación de una identidad falsa sin su consentimiento, utilizando tecnología para amenazar, intimidar o dañar su reputación. Creación de perfiles falsos en redes sociales. Uso de cuentas de correo electrónico con información o imágenes de otra persona para cometer fraudes, como robo de dinero o compras en línea."
+        },
+        hostigamiento: {
+            title: "Hostigamiento virtual",
+            description:
+                "Implica la comisión reiterada de actos abusivos y perturbadores a través de tecnología, con el objetivo de hostigar, intimidar, acechar, molestar, atacar, humillar, amenazar, asustar, ofender o abusar verbalmente de una víctima. Actos reiterados de hostigamiento, ataques, humillaciones, amenazas y ofensas mediante correos electrónicos, llamadas, mensajes de texto, chats en línea o plataformas de redes sociales."
+        },
+        acecho: {
+            title: "Acecho virtual",
+            description:
+                "Consiste en el rastreo constante de las actividades en línea y fuera de línea de una víctima, así como de su ubicación, desplazamientos e información, utilizando tecnología. Utilización de software espía en dispositivos electrónicos (sin consentimiento de la víctima), permitiendo el control remoto de cámaras o micrófonos en teléfonos móviles."
+        },
+        insultos: {
+            title: "Insultos electrónicos",
+            description:
+                "Implica el uso de tecnología para abusar, humillar, molestar, atacar, amenazar, degradar, intimidar, ofender o insultar a una persona, creando un ambiente ofensivo y hostil en los espacios digitales. Expresiones o comentarios discriminatorios. Envío de materiales sexualmente explícitos no solicitados (cyberflashing), como videos explícitos o fotografías ofensivas."
+        }
+    };
+
+    const termoValues = {
+        insultos: 0,
+        acecho: 12.5,
+        hostigamiento: 18.75,
+        suplantacion: 25,
+        difamacion: 31,
+        sextorsion: 37.5,
+        difusion: 43.5,
+        trata: 50
+    }
+
+    const termoColors = {
+        trata: '#ff2a40',
+        difusion: '#ff5742',
+        sextorsion: '#f27036',
+        difamacion: '#ffa70d',
+        suplantacion: '#ffcf09',
+        hostigamiento: '#bbc45b',
+        acecho: '#45b59d',
+        insultos: '#0090bd',
+    }
+
+    interface Description {
+        title: string,
+        description: string
+    }
+
+    const [temperature, setTemperature] = useState<number>(0);
+    const [temperatureHeight, setTemperatureHeight] = useState<string>('0%');
+    const [topic, setTopic] = useState<Description>(termoDescriptions.insultos)
+    const [topicColor, setTopicColor] = useState<string>(termoColors.insultos)
+    const [selected, setSelected] = useState<number>(0)
+
+    const calculateHeight = () => {
+        const height =
+            ((temperature - config.minTemp) / (config.maxTemp - config.minTemp)) *
+            100;
+        setTemperatureHeight(`${height}%`);
+    };
+
+    const handleClick = (topic: string) => {
+        setTemperature(termoValues[topic as keyof typeof termoValues]);
+        setTopic(termoDescriptions[topic as keyof typeof termoDescriptions]);
+        setTopicColor(termoColors[topic as keyof typeof termoColors]);
+        switch (topic) {
+            case 'trata': setSelected(1);
+                break;
+            case 'difusion': setSelected(2)
+                break;
+            case 'sextorsion': setSelected(3)
+                break;
+            case 'difamacion': setSelected(4)
+                break;
+            case 'suplantacion': setSelected(5)
+                break;
+            case 'hostigamiento': setSelected(6)
+                break;
+            case 'acecho': setSelected(7)
+                break;
+            case 'insultos': setSelected(8)
+                break;
+        }
+    }
+
+    useEffect(() => {
+        calculateHeight();
+    }, [temperature, config]);
+
+    return (
+        <Wrapper>
+            <Thermometer>
+                <Temperature
+                    height={temperatureHeight}
+                />
+                <Bulb />
+            </Thermometer>
+
+            <div className="options">
+                {topicList.map((topic, i) => {
+                    return (<div key={i} onClick={() => handleClick(topic[0])}>
+                        <p style={selected === i + 1 ? { fontWeight: 'bold' } : {}}>
+                            {topic[1]}
+                        </p>
+                    </div>)
+                })}
+
+            </div>
+            <Description className="description" color={topicColor}>
+                <h1>{topic.title}</h1>
+                <p>{topic.description}</p>
+            </Description>
+        </Wrapper>
+    );
+};
+
 interface TemperatureProps {
-    height: number,
-    dataValue: string
+    height: string
 }
 
-// Styled Components
 const Wrapper = styled.div`
   display: flex;
   border-radius: 2rem;
@@ -65,7 +219,6 @@ const Temperature = styled.div<TemperatureProps>`
   transition: height 1s ease-in-out;
   height: ${(props) => props.height}; /* Adjust visible height */
   background: linear-gradient(
-
     #ff2a40 0%,     
     #ff2a40 12.5%,  
     #ff5742 12.5%, 
@@ -87,18 +240,6 @@ const Temperature = styled.div<TemperatureProps>`
   background-position: bottom; /* Anchor the gradient at the bottom */
   background-repeat: no-repeat;
   overflow: hidden; /* Ensure only the visible portion of the gradient is shown */
-  &:before {
-    content: '${(props) => props.dataValue}';
-    position: absolute;
-    top: -30px;
-    left: 100%;
-    transform: translateX(-50%);
-    background: rgba(0, 0, 0, 0.7);
-    color: white;
-    padding: 5px 10px;
-    border-radius: 5px;
-    font-size: 1em;
-  }
 
   /* Media Query for Smaller Screens */
   @media (max-width: 800px) {
@@ -127,21 +268,8 @@ const Temperature = styled.div<TemperatureProps>`
     background-repeat: no-repeat;
     overflow: hidden; /* Clip overflowing content */
 
-    &:before {
-      content: '${(props) => props.dataValue}';
-      position: absolute;
-      top: -30px;
-      left: 100%;
-      transform: translateX(-50%);
-      background: rgba(0, 0, 0, 0.7);
-      color: white;
-      padding: 5px 10px;
-      border-radius: 5px;
-      font-size: 1em;
-    }
   }
 `;
-
 
 const Description = styled.div`
       border: solid black 2px;
@@ -188,169 +316,5 @@ const Description = styled.div`
         }
     }
 `
-
-
-// Main Component
-const ThermometerApp = () => {
-    const units = { Celcius: '°C', Fahrenheit: '°F' };
-    const [config, setConfig] = useState({
-        minTemp: 0,
-        maxTemp: 50,
-        unit: 'Celcius',
-    });
-    const termoDescriptions = {
-        trata: {
-            title: "Trata virtual de personas",
-            description:
-                "Conlleva la intermediación de las tecnologías para el ejercicio de poder sobre una víctima con el objeto de cometer abuso o explotación sexual de su imagen y/o de su cuerpo contra su voluntad. Uso de juegos en línea para atraer víctimas. Enganche y manipulación emocional. Seleccionan a víctimas más vulnerables: mujeres, niños, niñas."
-        },
-        difusion: {
-            title: "Difusión de contenido íntimo sin consentimiento",
-            description:
-                "Consiste en la difusión en línea no consensuada de imágenes íntimas obtenidas con o sin el consentimiento de la persona, con el propósito de avergonzar, estigmatizar o perjudicar a la víctima. Existencia de material audiovisual (real o editado) de carácter íntimo y/o sexual. Creación, almacenamiento, manipulación, producción, difusión, publicación, distribución, facilitación, cesión o entrega a terceros de este material."
-        },
-        sextorsion: {
-            title: "Sextorsión",
-            description:
-                "Implica el envío o publicación de comunicaciones o contenidos digitales que le anticipan a una persona la intención de cometer en su contra un daño físico o violencia sexual, o en contra de sus familiares, amistades o bienes. Ataques sexuales organizados o planificados mediante el uso de tecnologías, redes o sitios de citas. Presión sobre una persona para forzarla a actuar de cierto modo, obtener dinero, y/o amenazas de difundir información privada para chantaje sexual."
-        },
-        difamacion: {
-            title: "Difamación virtual",
-            description:
-                "Implica la creación, manipulación y publicación de información personal falsa, manipulada o fuera de contexto con la intención de descalificar o dañar la reputación de una persona. Creación de perfiles falsos en redes sociales o sitios en línea para afectar la reputación de la víctima. Publicación de publicidad falsa en sitios de citas o pornográficos. Uso y manipulación no consentida de información personal (fotos y videos)."
-        },
-        suplantacion: {
-            title: "Suplantación virtual",
-            description:
-                "Consiste en la utilización de la imagen, información o datos de una persona o la creación de una identidad falsa sin su consentimiento, utilizando tecnología para amenazar, intimidar o dañar su reputación. Creación de perfiles falsos en redes sociales. Uso de cuentas de correo electrónico con información o imágenes de otra persona para cometer fraudes, como robo de dinero o compras en línea."
-        },
-        hostigamiento: {
-            title: "Hostigamiento virtual",
-            description:
-                "Implica la comisión reiterada de actos abusivos y perturbadores a través de tecnología, con el objetivo de hostigar, intimidar, acechar, molestar, atacar, humillar, amenazar, asustar, ofender o abusar verbalmente de una víctima. Actos reiterados de hostigamiento, ataques, humillaciones, amenazas y ofensas mediante correos electrónicos, llamadas, mensajes de texto, chats en línea o plataformas de redes sociales."
-        },
-        acecho: {
-            title: "Acecho virtual",
-            description:
-                "Consiste en el rastreo constante de las actividades en línea y fuera de línea de una víctima, así como de su ubicación, desplazamientos e información, utilizando tecnología. Utilización de software espía en dispositivos electrónicos (sin consentimiento de la víctima), permitiendo el control remoto de cámaras o micrófonos en teléfonos móviles."
-        },
-        insultos: {
-            title: "Insultos electrónicos",
-            description:
-                "Implica el uso de tecnología para abusar, humillar, molestar, atacar, amenazar, degradar, intimidar, ofender o insultar a una persona, creando un ambiente ofensivo y hostil en los espacios digitales. Expresiones o comentarios discriminatorios. Envío de materiales sexualmente explícitos no solicitados (cyberflashing), como videos explícitos o fotografías ofensivas."
-        }
-    };
-    const termoValues = {
-        insultos: 0,
-        acecho: 12.5,
-        hostigamiento: 18.75,
-        suplantacion: 25,
-        difamacion: 31,
-        sextorsion: 37.5,
-        difusion: 43.5,
-        trata: 50
-    }
-
-    const termoColors = {
-        trata: '#ff2a40',
-        difusion: '#ff5742',
-        sextorsion: '#f27036',
-        difamacion: '#ffa70d',
-        suplantacion: '#ffcf09',
-        hostigamiento: '#bbc45b',
-        acecho: '#45b59d',
-        insultos: '#0090bd',
-    }
-
-    const [temperature, setTemperature] = useState(0);
-    const [temperatureHeight, setTemperatureHeight] = useState('0%');
-    const [topic, setTopic] = useState(termoDescriptions.insultos)
-    const [topicColor, setTopicColor] = useState(termoColors.insultos)
-    const [selected, setSelected] = useState(0)
-
-    const calculateHeight = () => {
-        const height =
-            ((temperature - config.minTemp) / (config.maxTemp - config.minTemp)) *
-            100;
-        setTemperatureHeight(`${height}%`);
-    };
-
-    const handleClick = (topic: string) => {
-        setTemperature(termoValues[topic])
-        setTopic(termoDescriptions[topic])
-        setTopicColor(termoColors[topic])
-        switch (topic) {
-            case 'trata': setSelected(1);
-                break;
-            case 'difusion': setSelected(2)
-                break;
-            case 'sextorsion': setSelected(3)
-                break;
-            case 'difamacion': setSelected(4)
-                break;
-            case 'suplantacion': setSelected(5)
-                break;
-            case 'hostigamiento': setSelected(6)
-                break;
-            case 'acecho': setSelected(7)
-                break;
-            case 'insultos': setSelected(8)
-                break;
-        }
-    }
-
-    useEffect(() => {
-        calculateHeight();
-    }, [temperature, config]);
-
-
-    return (
-        <Wrapper>
-            <Thermometer>
-                <Temperature
-                    height={temperatureHeight}
-                    dataValue={`${temperature}${units[config.unit]}`}
-                />
-                <Bulb />
-            </Thermometer>
-
-            <div className="options">
-                <div onClick={() => handleClick('trata')}>
-                    <p style={selected === 1 ? { fontWeight: 'bold' } : {}}>
-                        Trata virtual de personas
-                    </p>
-                </div>
-                <div onClick={() => handleClick('difusion')}>
-
-                    <p style={selected === 2 ? { fontWeight: 'bold' } : {}}>Difusión de contenido íntimo sin consentimiento</p>
-                </div>
-                <div onClick={() => handleClick('sextorsion')}>
-                    <p style={selected === 3 ? { fontWeight: 'bold' } : {}}>Sextorsión</p></div>
-                <div onClick={() => handleClick('difamacion')}>
-
-                    <p style={selected === 4 ? { fontWeight: 'bold' } : {}}>Difamación virtual</p>
-                </div>
-                <div onClick={() => handleClick('suplantacion')}>
-
-                    <p style={selected === 5 ? { fontWeight: 'bold' } : {}}>Suplantación virtual</p>
-                </div>
-                <div onClick={() => handleClick('hostigamiento')}>
-
-                    <p style={selected === 6 ? { fontWeight: 'bold' } : {}}>Hostigamiento virtual</p>
-                </div>
-                <div onClick={() => handleClick('acecho')}>
-                    <p style={selected === 7 ? { fontWeight: 'bold' } : {}}>Acecho virtual</p></div>
-                <div onClick={() => handleClick('insultos')}>
-
-                    <p style={selected === 8 ? { fontWeight: 'bold' } : {}}>Insultos electrónicos</p>
-                </div>
-            </div>
-            <Description className="description" color={topicColor}>
-                <h1>{topic.title}</h1>
-                <p>{topic.description}</p>
-            </Description>
-        </Wrapper>
-    );
-};
 
 export default ThermometerApp;
